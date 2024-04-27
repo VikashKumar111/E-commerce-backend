9f98287758ace0bd2867177c12b946e852bd6e9d
Vikash Kumar <vikasthakur9310@gmail.com>
2024-04-27 15:10:22 +0530

    add

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,6 +3,19 @@ const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 
 const authMiddleware = asyncHandler(async (req, res, next) => {
-    
+    let token;
+    if (req?.headers?.authorization?.startsWith("Bearer")) {
+        token = req.header.authorization.split(" ")[1];
+        try {
+            if (token) {
+                const decoded = jwt.verify(token, process.env.JWT_SECRET);
+                console.log(decoded);
+           } 
+        } catch (error) {
+            throw new Error("Not Authorized token expired , Please login again");
+        }
+    } else {
+        throw new Error("There is no token attached to the header");
+    }
 }
 );
