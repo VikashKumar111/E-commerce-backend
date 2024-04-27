61843a57eb42f302e486dcbac44e491b088c4792
Vikash Kumar <vikasthakur9310@gmail.com>
2024-04-27 18:13:00 +0530

    add

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,11 +5,13 @@ const asyncHandler = require("express-async-handler");
 const authMiddleware = asyncHandler(async (req, res, next) => {
     let token;
     if (req?.headers?.authorization?.startsWith("Bearer")) {
-        token = req.header.authorization.split(" ")[1];
+        token = req.headers.authorization.split(" ")[1];
         try {
             if (token) {
                 const decoded = jwt.verify(token, process.env.JWT_SECRET);
-                console.log(decoded);
+                const user = await User.findById(decoded?.id);
+                req.user = user;
+                next();
            } 
         } catch (error) {
             throw new Error("Not Authorized token expired , Please login again");
@@ -19,3 +21,9 @@ const authMiddleware = asyncHandler(async (req, res, next) => {
     }
 }
 );
+
+
+const isAdmin = asyncHandler(async (req, res, next) => {
+    console.log(req.user);
+})
+module.exports = { authMiddleware ,isAdmin };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { createUser, loginUserCtrl ,getAllUser, getaUser, deleteaUser, updatedUser} = require("../controller/userCtrl");
+const { authMiddleware } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 
 router.post("/register", createUser);
 router.post("/login", loginUserCtrl);
 router.get("/all-users", getAllUser);
-router.get("/:id", getaUser);
+router.get("/:id",authMiddleware, getaUser);
 router.delete("/:id", deleteaUser);
-router.put("/:id", updatedUser);
+router.put("/edit-user", updatedUser);
 module.exports = router;
