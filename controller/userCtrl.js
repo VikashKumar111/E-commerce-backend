a3f86c5051d3e8f09c3260ad28df45c73ca228be
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-03 13:24:49 +0530

    add

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -4,6 +4,8 @@ const asyncHandler = require("express-async-handler");
 const validateMongoDbId = require("../utils/validateMongodbid");
 const { generateRefreshToken } = require("../config/refreshToken");
 const jwt = require("jsonwebtoken");
+const sendEmail = require("./emailCtrl");
+const crypto = require("crypto");
 
 const createUser = asyncHandler(async (req, res) => {
   const email = req.body.email;
@@ -224,12 +226,29 @@ const updatePassword = asyncHandler(async (req, res) => {
 const forgotPasswordToken = asyncHandler(async (req, res) => {
   const { email } = req.body;
   const user = await User.findOne({ email });
-  if (!user) throw new Error("user not found with this email");
+  if (!user) throw new Error("User not found with this email");
   try{
-    
+    const token = await user.createPasswordResetToken();
+    await user.save();
+    const resetURL = `Hi, Please follow this link to reset your password. This link is valid till 10 minutes from now.<a href='http://localhost:5000/api/user/reset-password/${token}'>Click Here</a>`;
+    const data = {
+      to: email,
+      text: "Hey User",
+      subject: "Forgot Password Link",
+      htm: resetURL,
+    }
+    sendEmail(data);
+    res.json(token);
   } catch (error) {
-    throw new Error(error);
+    throw new Error(error);  
   }
+});
+
+const resetpassword = asyncHandler(async (req, res) => {
+  const { password } = req.body;
+  const { token } = req.params;
+  const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
+
 });
 module.exports = {
   createUser,
