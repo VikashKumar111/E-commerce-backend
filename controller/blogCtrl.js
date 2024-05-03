6f18c004f8f9f192fb132732766e8fd596449928
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-03 17:52:33 +0530

    add

---
+++ controller/blogCtrl.js
@@ -0,0 +1,11 @@
+const Blog = require("../models/blogModel");
+const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
+const validateMongoDbId = require("../utils/validateMongodbid");
+
+
+const createBlog = asyncHandler(async (req, res) => {
+    
+});
+
+module.exports = { createBlog };
