ddaed0643a0e29d5485109166f4e3696771e471a
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-03 11:35:32 +0530

    add

---
+++ controller/emailCtrl.js
@@ -0,0 +1,9 @@
+const nodemailer = require("nodemailer");
+const asyncHandler = require("express-async-handler");
+
+
+const sendEmail = asyncHandler(async (data, req, res) => {
+    
+});
+
+module.exports = sendEmail;
