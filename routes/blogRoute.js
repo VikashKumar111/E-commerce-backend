4dbead8d3376d772120a423237149509a055e5af
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-04 12:58:55 +0530

    add

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { createBlog } = require("../controller/blogCtrl");
+const { createBlog, updateBlog } = require("../controller/blogCtrl");
 const router = express.Router();
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 
 router.post('/', authMiddleware, isAdmin, createBlog);
-router.put('/:id', authMiddleware, isAdmin, createBlog);
+router.put('/:id', authMiddleware, isAdmin, updateBlog);
 module.exports = router;
 
 
