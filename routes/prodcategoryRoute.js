bedee692ab093feb3f887f9bd10242dbe5bcf46b
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-07 15:28:55 +0530

    add

--- routes/prodcategoryRoute.js
+++ routes/prodcategoryRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { createCategory } = require("../controller/prodcategoryCtrl");
+const { createCategory, updateCategory, deleteCategory } = require("../controller/prodcategoryCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 
-router.post("/" ,authMiddleware, isAdmin, createCategory)
+router.post("/", authMiddleware, isAdmin, createCategory);
+router.put("/:id", authMiddleware, isAdmin, updateCategory);
+router.delete("/:id", authMiddleware, isAdmin, deleteCategory);
 
 module.exports = router;
