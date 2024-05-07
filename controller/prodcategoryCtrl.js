5ec89599148b52bd365740a7f31bfac1758faad4
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-07 12:41:23 +0530

    add

---
+++ controller/prodcategoryCtrl.js
@@ -0,0 +1,7 @@
+const Category = require("../models/prodcategoryModel");
+const asyncHandler = require("express-async-handler");
+const validateMongoDbId = require("../utils/validateMongodbid");
+
+
+
+module.exports = {};
