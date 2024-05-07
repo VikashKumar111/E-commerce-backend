280eb7b9744168667984fa54220d33fc332cf00e
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-07 13:23:41 +0530

    add

--- controller/prodcategoryCtrl.js
+++ controller/prodcategoryCtrl.js
@@ -3,5 +3,14 @@ const asyncHandler = require("express-async-handler");
 const validateMongoDbId = require("../utils/validateMongodbid");
 
 
+const createCategory = asyncHandler(async (req, res) => {
+    try {
+        const newCategory = await Category.create(req.body);
+        res.json(newCategory);
+        
+    } catch (error) {
+        throw new Error(error)
+    }
+});
 
-module.exports = {};
+module.exports = {createCategory,};
--- routes/prodcategoryRoute.js
+++ routes/prodcategoryRoute.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const { createCategory } = require("../controller/prodcategoryCtrl");
+const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 
-
+router.post("/" ,authMiddleware, isAdmin, createCategory)
 
 module.exports = router;
