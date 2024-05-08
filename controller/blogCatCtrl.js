8c5c36b9dba995ada2860f3f308c7833ea0ed451
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-08 17:41:07 +0530

    add

--- controller/blogCatCtrl.js
+++ controller/blogCatCtrl.js
@@ -1,40 +1,48 @@
-const Category = require('../models/blogCatModel');
-const asyncHandler = require('express-async-handler');
+const Category = require("../models/blogCatModel");
+const asyncHandler = require("express-async-handler");
 const validateMongoDbId = require("../utils/validateMongodbId");
 
-
 const createCategory = asyncHandler(async (req, res) => {
-    try {
-        const createCategory = await Category.create(req.body);
-        res.json(createCategory);
-    } catch (error) {
-        throw new Error(error);
-    }
+  try {
+    const createCategory = await Category.create(req.body);
+    res.json(createCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
 });
 
-
 const updateCategory = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    validateMongoDbId(id);
-    try {
-        const updatedCategory = await Category.findByIdAndUpdate(id, req.body, {
-            new: true,
-        });
-        res.json(updatedCategory);
-    } catch (error){
-        throw new Error(error); 
-    }
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const updatedCategory = await Category.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updatedCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
 });
 
 const deleteCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const deletedCategory = await Category.findByIdAndDelete(id);
+    res.json(deletedCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const getCategory = asyncHandler(async (req, res) => {
     const { id } = req.params;
     validateMongoDbId(id);
     try {
-        const deletedCategory = await Category.findByIdAndDelete(id);
-        res.json(deletedCategory);
+        const getaCategory = await Category.findById(id);
+        res.json(getaCategory);
     } catch (error) {
         throw new Error(error);
     }
-
 });
-module.exports = {createCategory, updateCategory, deleteCategory};
+module.exports = { createCategory, updateCategory, deleteCategory , getCategory };
