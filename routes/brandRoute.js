0459d3a61c88bd62c1c2ca3c56cea5cf9ec490b2
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-09 22:51:59 +0530

    add

--- routes/brandRoute.js
+++ routes/brandRoute.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const { createBrand, updateBrand } = require("../controller/brandCtrl");
+const { createBrand, updateBrand, deleteBrand, getBrand, getallBrand } = require("../controller/brandCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/", authMiddleware, isAdmin, createBrand);
 router.put("/:id", authMiddleware, isAdmin, updateBrand);
+router.delete("/:id", authMiddleware, isAdmin, deleteBrand);
+router.get("/:id", getBrand);
+router.get("/", getallBrand);
 
 
 module.exports = router;
