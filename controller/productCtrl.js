b94c72418281d576e8ff5b93917be94df043d55a
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-10 17:08:57 +0530

    add

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -133,39 +133,7 @@ const addToWishlist = asyncHandler(async (req, res) => {
 });
 
 
-// const addToWishlist = asyncHandler(async (req, res) => {
-//   const { _id } = req.user;
-//   const { prodId } = req.body;
-//   try {
-//     const user = await User.findById(_id);
-//     const alreadyadded = user.wishlist.find((id) => id.toString() === prodId);
-//     if (alreadyadded) {
-//       let user = await User.findByIdAndUpdate(
-//         _id,
-//         {
-//           $pull: { wishlist: prodId },
-//         },
-//         {
-//           new: true,
-//         }
-//       );
-//       res.json(user);
-//     } else {
-//       let user = await User.findByIdAndUpdate(
-//         _id,
-//         {
-//           $push: { wishlist: prodId },
-//         },
-//         {
-//           new: true,
-//         }
-//       );
-//       res.json(user);
-//     }
-//   } catch (error) {
-//     throw new Error(error);
-//   }
-// });
+
 
 module.exports = {
   createProduct,
