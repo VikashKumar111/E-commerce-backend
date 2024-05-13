e304d8ac237dffac2d7ad664ac6f3f66ee6c8e67
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-13 16:22:52 +0530

    add

--- controller/couponCtrl.js
+++ controller/couponCtrl.js
@@ -2,20 +2,37 @@ const Coupon = require("../models/couponModel");
 const asyncHandler = require("express-async-handler");
 const validateMongoDbId = require("../utils/validateMongodbId");
 
-
 const createCoupon = asyncHandler(async (req, res) => {
+  try {
+    const newCoupon = await Coupon.create(req.body);
+    res.json(newCoupon);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const getAllCoupans = asyncHandler(async (req, res) => {
+  try {
+    const coupons = await Coupon.find();
+    res.json(coupons);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const getCoupan = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    validateMongoDbId(id);
     try {
-        const newCoupon = await Coupon.create(req.body);
-        res.json(newCoupon);
+        const getaCoupon = await Coupon.findById(id);
+        res.json(getaCoupon);
     } catch (error) {
         throw new Error(error);
     }
-});
-
-
-
-
+})
 
 module.exports = {
-   createCoupon, 
+  createCoupon,
+    getAllCoupans,
+  getCoupan,
 };
--- routes/couponRoute.js
+++ routes/couponRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { createCoupon } = require("../controller/couponCtrl");
+const { createCoupon, getAllCoupans } = require("../controller/couponCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 
-router.post("/",authMiddleware, isAdmin, createCoupon);
+router.post("/", authMiddleware, isAdmin, createCoupon);
+router.get("/",authMiddleware, isAdmin, getAllCoupans);
 
 
 module.exports = router;
