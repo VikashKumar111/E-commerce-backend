902abb725be56deafce8640d7703bb4de718c654
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-14 16:44:03 +0530

    add

--- middlewares/uploadImage.js
+++ middlewares/uploadImage.js
@@ -13,7 +13,13 @@ const multerStorage = multer.diskStorage({
     },
 });
 
-
+const multerFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith("image")) {
+        cb(null, true);
+    } else {
+        cb({ message: "Unsupported file format" }, false);
+    }
+};
 
 
 
