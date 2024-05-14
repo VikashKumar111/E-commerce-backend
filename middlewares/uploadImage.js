63d357177b70bb1f28c15a65ff3edfd7dcca6c5f
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-14 16:29:25 +0530

    add

--- middlewares/uploadImage.js
+++ middlewares/uploadImage.js
@@ -3,6 +3,19 @@ const sharp = require("sharp");
 const path = require("path");
 
 
+const multerStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, "../public/images/"));
+    },
+    filename: function (req, file, cb) {
+        const uniquesuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+        cb(null, file.fieldname + "-" + uniquesuffix + ".jpeg");
+    },
+});
+
+
+
+
 
 const uploadPhoto = multer({
     storage: multerStorage,
