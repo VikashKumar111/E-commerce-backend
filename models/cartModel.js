f2c122afca8fa4c000a445ac205674471cc641f1
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-16 18:42:24 +0530

    add

--- models/cartModel.js
+++ models/cartModel.js
@@ -1,28 +1,30 @@
 const mongoose = require("mongoose"); // Erase if already required
 
 // Declare the Schema of the Mongo model
-var cartSchema = new mongoose.Schema({
-  products: [
-    {
-      product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
+var cartSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        count: Number,
+        color: String,
+        price: Number,
       },
-      count: Number,
-      color: String,
-      price: Number,
+    ],
+    cartTotal: Number,
+    totalAfterDiscount: Number,
+    orderby: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
-  ],
-  cartTotal: Number,
-  totalAfterDiscount: Number,
-  orderby: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
   },
-},
-    {
-    timestamps:true,
-});
+  {
+    timestamps: true,
+  }
+);
 
 //Export the model
 module.exports = mongoose.model("Cart", cartSchema);
