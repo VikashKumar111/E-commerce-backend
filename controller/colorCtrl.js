c3b147a7910992f564a13d6269c223af50cbc846
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-20 12:23:00 +0530

    add

--- controller/colorCtrl.js
+++ controller/colorCtrl.js
@@ -25,4 +25,17 @@ const updateColor = asyncHandler(async (req, res) => {
     throw new Error(error);
   }
 });
-module.exports = { createColor, updateColor };
+
+const deleteColor = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    validateMongoDbId(id);
+
+    try {
+        const deletedColor = await Color.findByIdAndDelete(id);
+        res.json(deletedColor);
+    } catch (error) {
+        throw new Error(error);
+    }
+
+})
+module.exports = { createColor, updateColor ,deleteColor};
--- routes/colorRoute.js
+++ routes/colorRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { createColor, updateColor } = require("../controller/colorCtrl");
+const { createColor, updateColor, deleteColor } = require("../controller/colorCtrl");
 const router = express.Router();
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 
 router.post("/", authMiddleware, isAdmin, createColor);
 router.put("/:id",authMiddleware,isAdmin,updateColor)
+router.delete("/:id", authMiddleware, isAdmin, deleteColor);
 
 
 module.exports = router;
