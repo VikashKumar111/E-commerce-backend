2448a2c1c43a22e8e20c821d13e9e571b7b79185
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-20 11:06:59 +0530

    add

--- controller/colorCtrl.js
+++ controller/colorCtrl.js
@@ -1,10 +1,23 @@
+// ("")
+
+
 const Color = require("../models/colorModel");
 const asyncHandler = require("express-async-handler");
 const validateMongoDbId = require("../utils/validateMongodbid");
 
 
 
+const createColor = asyncHandler(async (req, res) => {
+    try {
+        const newColor = await Color.create(req.body);
+        res.json(newColor);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
 
 
 
-module.exports = {};
+
+
+module.exports = {createColor};
