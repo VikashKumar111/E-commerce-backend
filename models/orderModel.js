434226db90d6769fccca3f31b06f82f81d97685c
Vikash Kumar <vikasthakur9310@gmail.com>
2024-10-16 15:24:12 +0530

    updates

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,60 +1,91 @@
 const mongoose = require("mongoose"); // Erase if already required
 
 // Declare the Schema of the Mongo model
-var orderSchema = new mongoose.Schema(
-  {
-    products: [
-      {
-        product: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Product",
-        },
-        count: Number,
-        color: String,
-      },
-    ],
-    paymentIntent: {},
-    orderStatus: {
+var orderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  shippingInfo: {
+    firstName: {
       type: String,
-      default: "Not Processed",
-      enum: [
-        "Not Processes",
-        "Cash on Delivery",
-        "Processing",
-        "Dispatched",
-        "Cancelled",
-        "Delivered",
-      ],
-    },
-    orderby: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-    paidAt: {
-      type: Date,
-      default: Date.now(),
-    },
-    month: {
-      type: String,
-      default: new Date().getMonth(),
-    },
-    totalPrice: {
-      type: Number,
       required: true,
     },
-    totalPriceAfterDiscount: {
-      type: Number,
+    lastName: {
+      type: String,
       required: true,
     },
-    orderStatus: {
+    address: {
       type: String,
-      default: "Ordered",
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    other: {
+      type: String,
+      required: true,
+    },
+    pincode: {
+      type: Number,
+      required: true,
     },
   },
-  {
-    timestamps: true,
+  paymentInfo: {
+    razorpayOrderId: {
+      type: String,
+      required: true,
+    },
+    razorpayPaymentId: {
+      type: String,
+      required: true,
+    },
+  },
+  orderItems: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      color: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Color",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  paidAt: {
+    type:Date,
+    default:Date.now()
+  },
+  totalPrice: {
+    type: Number,
+    required:true
+  },
+  totalPriceAfterDiscount: {
+    type: Number,
+    required:true
+  },
+  orderStatus: {
+    type: String,
+    default:"Ordered"
   }
-);
+});
 
 //Export the model
 module.exports = mongoose.model("Order", orderSchema);
